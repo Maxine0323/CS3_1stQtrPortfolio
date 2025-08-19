adb3a3d4aaf160301d2c3affbeeac2e4544fc55b
Maxine0323 <b2029mtrepedro@pshs.edu.ph>
2025-08-19 17:57:33 +0800

    Second Graded Exercise

--- public/script.js
+++ public/script.js
@@ -4,3 +4,4 @@ let numPerDay = 3;
 totalRequired = (numPerDay * 365) * (maxAge - age);
 document.write("You will need " +totalRequired+ " kimchi to last you until the ripe old age of " +maxAge+ ".");
 
+
